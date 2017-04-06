9a45ff7a531d1e8983d077ee276ce3b5dc493b14
furkansahin <sahinffurkan@gmail.com>
2017-04-06 10:23:20 +0300

    Initial code for server uploads is done

---
+++ UploadUnit/src/main/java/UploadObjectSingleOperation.java
@@ -0,0 +1,51 @@
+/**
+ * Created by furkansahin on 05/04/2017.
+ */
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class UploadObjectSingleOperation {
+    private static String bucketName     = System.getenv("BUCKET_NAME");
+    private static String keyName        = "BomBom2";
+    private static String uploadFileName = "/Users/furkansahin/CrypDist/UploadUnit/src/main/java/cs442_clock.pdf";
+
+    public static void main(String[] args) throws IOException {
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .withRegion(Regions.EU_CENTRAL_1).build();
+        try {
+            System.out.println("Uploading a new object to S3 from a file\n");
+            File file = new File(uploadFileName);
+            s3client.putObject(new PutObjectRequest(
+                    bucketName, keyName, file));
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " +
+                    "means your request made it " +
+                    "to Amazon S3, but was rejected with an error response" +
+                    " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " +
+                    "means the client encountered " +
+                    "an internal error while trying to " +
+                    "communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+}
